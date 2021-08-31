e6b08e3e64ead82eca5cfdd822f869ad3f483564
Samuel <samlee1097@yahoo.com>
2021-08-31 15:08:28 -0400

    commit tree 0d80dd136fead58fa1e089fd710efc0f5b0dc24c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'Sam';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer() { 
+    bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer(newCustomer){
+    bestCustomer = newCustomer;
+};
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Joe';
+};
